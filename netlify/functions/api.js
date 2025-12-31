b20e627bf3753c8bf8b5d58bfe6d2a9ca5df6e25
manojmanoj143 <manojmanoj88680@gmail.com>
2025-12-31 14:10:42 +0530

    Fix 502: Align serverless-http version and add error logging

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,1 +1,12 @@
-exports.handler = require("../../server/index").handler;
+try {
+    const { handler } = require("../../server/index");
+    exports.handler = handler;
+} catch (error) {
+    console.error("Function Initialization Error:", error);
+    exports.handler = async () => {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Function Initialization Error", details: error.message, stack: error.stack }),
+        };
+    };
+}
